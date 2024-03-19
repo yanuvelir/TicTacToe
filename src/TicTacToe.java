ed4c895625874bd55c54cb4c7e7648d86dd329e7
yanuvelir <yanuvelir@gmail.com>
2024-03-19 16:02:30 +0200

    Created an empty methods

---
+++ src/TicTacToe.java
@@ -0,0 +1,41 @@
+import java.security.PublicKey;
+
+public class TicTacToe {
+
+    public static final String EMPTY = "   ", CROSS = " X ", ZERO = " 0 ";
+    public static final int ROWS = 3, COLUMNS = 3;
+    public static String[][] mesh = new String[COLUMNS][ROWS];
+    public static int gameStatus;
+    public static final int STATUS_GAME_GO_ON = 0, DEAD_HEAT = 1, STATUS_WIN_X = 3, STATUS_WINY = 4;
+
+    public static void main(String[] args){
+
+    }
+
+    public static void startGame(){
+
+    }
+
+    public static void retrieveGamersInput(){
+
+    }
+
+    public static void analyzeMesh(){
+
+    }
+
+    public static boolean meshIsCompleted(){
+
+        return;
+    }
+
+    public static String findTheWinner(){
+
+        return ;
+    }
+
+    public static void showTheMesh(){
+
+    }
+
+}
